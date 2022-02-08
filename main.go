e666605b6dd26aa86e0e56297984ff0a7c79040d
Kevin Chen <kevin.signal@gmail.com>
2022-02-08 08:11:55 -0600

    Added default configuration.

--- main.go
+++ main.go
@@ -10,10 +10,25 @@ package main
 type hidemeConfig struct {
 	randomFilenames  []string `json:"randomFilenames"`
 	randomExtentions []string `json:"randomExtensions"`
-	locationsTry     []string `json:"locationsTry"`
+	locationsTryWin  []string `json:"locationsTryWin"`
+	locationsTryUnix []string `json:"locationsTryUnix"`
 	split            int      `json:"split"`
 	passphrase       string   `json:"passphrase"`
 }
 
+defaultConfig hidemeConfig = hidemeConfig{
+	randomFilenames:  ["Thumbs", "auth", "tempDiagnostic", "Info", "SysInfo", "SysDiagnostic", "SysAuth", "meta", "metaInfo", "tempfile", "taskProcessMeta", "FileData", // dumb system files
+		"WData", "FData", "ITemp", "tempLog", "manifest", "infoLog", "metaData", "SysLog", "authConfig", "settings", "dump", "dumpTrace" // more dumb system files
+			  ],
+	randomExtensions: ["db", "tmp", "sys", "plist", "log", "info", "inf", "dll", "pkg", "spec", "mta", "dat", "bin"],
+	locationsTryWin:  ["c:/", "c:/temp", "c:/tmp", "c:/windows/Temp", "c:/users", "c:/users/%username%/AppData/", "c:/users/%username%/appdata/local",
+		"c:/logs", "c:/perflogs", "c:/%WinLogs", "c:/riot games"] // oh yes i sure do love people putting things in my valorant install folder
+	locationsTryUnix: ["/home/%username%/.local", "/home/%username%/.sysproc", "/home/%username%/", "/etc/.configdata/", "/etc/.tempinfo/"] // hidden linux stuff
+}
+
+
+// get a random file name
+func getFileNames
 
 // load the config
+func loadConfigFile
